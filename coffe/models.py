5b3193bee5485fcdc19eca3cde3a9272a05ec71e
juan alvarez <juan.alvarez.g.97@gmail.com>
2024-06-22 02:21:55 -0400

    se creo los modelo de producto y categoria, y se registro en el admin de django con el super usuario

--- coffe/models.py
+++ coffe/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+class Categoria(models.Model):
+
+    id_categoria =models.AutoField(db_column='idCate',primary_key=True)
+    nombre=models.CharField(max_length=35)
+    def __str__(self):
+        return str(self.nombre)
+
+
+
+class Producto(models.Model):
+
+    id_producto=models.AutoField(db_column='idProd',primary_key=True)
+    nombre=models.CharField(max_length=350)
+    imagen=models.ImageField(upload_to='upload')
+    descripcion=models.CharField(max_length=400)
+    precio=models.IntegerField()
+    fk_categoria=models.ForeignKey('Categoria',on_delete=models.CASCADE,db_column='idCate')
+
+
+
+    def __str__(self):
+        return str(self.nombre)
--- coffe/views.py
+++ coffe/views.py
@@ -5,3 +5,7 @@ from django.shortcuts import render
 def index(request):
 
     return render(request,'coffe/index.html')
+
+def producto(request):
+
+    return render(request,'coffe/productos.html')
