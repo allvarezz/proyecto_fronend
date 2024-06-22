e908c247fc8ace34b2982907f0fca82b25e87791
juan alvarez <juan.alvarez.g.97@gmail.com>
2024-06-22 02:29:16 -0400

    se hizo un registro de producto y categoria por el admin y visualisando esos datos por una vista personalizada con un for

--- coffe/views.py
+++ coffe/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Producto
 
 # Create your views here.
 
@@ -7,5 +8,8 @@ def index(request):
     return render(request,'coffe/index.html')
 
 def producto(request):
+    productos=Producto.objects.all()
 
-    return render(request,'coffe/productos.html')
+    return render(request,'coffe/productos.html',{
+        'productos':productos
+    })
