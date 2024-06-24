9bbcf7bf41df107c108992ce4746aeba356e1d72
juan alvarez <juan.alvarez.g.97@gmail.com>
2024-06-23 20:38:16 -0400

    deje listo el agregar y eliminar

--- coffe/urls.py
+++ coffe/urls.py
@@ -6,5 +6,6 @@ urlpatterns=[
     path('productos/',views.producto,name='productos'),
     path('add/',views.add_prod,name='add'),
     path('BD/',views.list_prod,name='BD'),
+    path('BD/<str:id>',views.del_prod,name='dele'),
 
 ]
--- coffe/views.py
+++ coffe/views.py
@@ -8,6 +8,10 @@ def index(request):
 
     return render(request,'coffe/index.html')
 
+def Ext_prod(request):
+     productos=Producto.objects.all()
+     return productos
+
 def producto(request):
     productos=Producto.objects.all()
 
@@ -20,6 +24,28 @@ def list_prod(request):
 
      return render(request,'coffe/listado_prod.html',{'productos':productos})
 
+def del_prod(request,id):
+     
+        try:
+          productos=Producto.objects.all()
+          producto=Producto.objects.get(id_producto=int(id))
+          producto.delete()
+          return render(request,'coffe/listado_prod.html',{'productos':productos})
+        except:
+             return render(request,'coffe/listado_prod.html',{'productos':productos})
+
+def edi_prod(request,id):
+        try:
+          productos=Producto.objects.all()
+          producto=Producto.objects.get(id_producto=int(id))
+          if producto:
+               if request.method == 'POST':
+                    form=FormProd(request.POST,instance=producto)
+                    form.save()
+          return redirect('BD')
+        except:
+             return render(request,'coffe/listado_prod.html',{'productos':productos})
+     
 def add_prod(request):
     if request.method == 'POST':
         form=FormProd(request.POST,request.FILES)
